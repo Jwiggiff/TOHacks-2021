183b1f32282f607846a2f35ddca7b3e8b466d0bd
Jwiggiff <friedman.josh03@gmail.com>
2021-05-08 16:49:55 -0400

    do a thing

---
+++ data-services-auth.js
@@ -0,0 +1,21 @@
+//set up schemas here
+
+
+
+// need these functions. Note: use promises to resolve() and reject() when returning.
+module.exports = {
+    initialize: function(){
+        //initializing functions. e.g. create connection, create model etc.
+        
+    },
+
+    checkUser: function(userData){
+        // checks if user is in database
+
+    },
+
+    registerUser: function(userData){
+        // passed in a user object with all the properties in your schema, just put this person into your database. oh, but also check if there's already a person.
+    }
+
+}
--- server.js
+++ server.js
@@ -1,6 +1,59 @@
 const express = require("express");
 const app = express();
+var path = require("path");
 
-app.use(express.static("public"));
+const exphbs = require("express-handlebars");
+const sessions = require("client-sessions");
+
+app.engine(".hbs", exphbs({ extname: ".hbs", defaultLayout: "default" }));
+app.set("view engine", ".hbs");
+
+app.use(
+  sessions({
+    cookieName: "authCookie",
+    secret: "JohnCena",
+    duration: 2 * 60 * 1000,
+    activeDuration: 1000 * 60,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.locals.sessions = req.session;
+  next();
+});
+
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    // Not logged in
+    res.redirect("/landing");
+  } else {
+    next();
+  }
+}
+
+// GET Methods ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+app.get("/", function (req, res) {
+  res.render("landing");
+});
+
+app.get("/dashboard", ensureLogin, function (req, res) {
+  res.render("dashboard");
+});
+
+
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+
+
+// initialize ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+app.use(express.static("public"));
+
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+};
+
+app.use(function(req,res){
+  res.status(404).send("Page Not Found");
+});
+
