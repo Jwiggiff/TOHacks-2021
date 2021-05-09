779e3393c33ceff9e7eedf6e31ac5eaa6fd544ac
Om Patel <omupatel2003@gmail.com>
2021-05-09 03:00:21 -0400

    Update data-services-auth.js

--- data-services-auth.js
+++ data-services-auth.js
@@ -98,7 +98,7 @@ module.exports = {
     },
     registerUser: function(userData){
         return new Promise((resolve,reject)=>{
-            pool.query(`INSERT INTO data.users VALUES (default, '${userData.fname}', '${userData.lname}', '${userData.email}', '${userData.password}', NULL, 0, 1, NULL, NULL, NULL, NULL);`, (err, res) => {
+            pool.query(`INSERT INTO data.users VALUES (default, '${userData.fname}', '${userData.lname}', '${userData.email}', '${userData.password}', 0, 0, 1, NULL, NULL, NULL, 0, NULL);`, (err, res) => {
                 if (err) {
                     console.log('Something went wrong. Please ensure the user data was inputted correctly.');
                     console.error(err);
