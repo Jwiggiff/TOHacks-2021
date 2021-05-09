3f0581f73b44b2c4215fcecbe27c20841850e53d
Jwiggiff <friedman.josh03@gmail.com>
2021-05-09 00:26:02 -0400

    Add rewards page

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 var path = require("path");
-var dataServicesAuth = require("./data-services-auth")
-var dataService = require("./data-service")
+var dataServicesAuth = require("./data-services-auth");
+var dataService = require("./data-service");
 app.use(express.urlencoded({ extended: false }));
-
 
 const exphbs = require("express-handlebars");
 const sessions = require("client-sessions");
@@ -35,6 +34,15 @@ function ensureLogin(req, res, next) {
   }
 }
 
+function ensureLoggedOut(req, res, next) {
+  if (req.session.user) {
+    // Logged in
+    res.redirect("/dashboard");
+  } else {
+    next();
+  }
+}
+
 // GET Methods ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 app.get("/", (req, res) => {
@@ -45,24 +53,28 @@ app.get("/dashboard", ensureLogin, function (req, res) {
   res.render("dashboard");
 });
 
-app.get("/login", function (req, res) {
+app.get("/login", ensureLoggedOut, function (req, res) {
   res.render("login", {
-    email: req.query.email
+    email: req.query.email,
   });
 });
 app.get("/register", function (req, res) {
   res.render("register");
 });
-app.get("/logout", function(req, res) {
+app.get("/logout", function (req, res) {
   req.session.reset();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-app.get("/explore", function(req, res) {
+app.get("/explore", function (req, res) {
   res.render("explore");
 });
 
-app.get("/settings", function(req, res) {
+app.get("/rewards", function (req, res) {
+  res.render("rewards");
+});
+
+app.get("/settings", function (req, res) {
   res.render("settings");
 });
 
@@ -71,44 +83,41 @@ app.get("/settings", function(req, res) {
 const user = {
   username: "sampleuser",
   password: "samplepassword",
-  email: "sampleuser@example.com"
+  email: "sampleuser@example.com",
 };
 
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if(email === "" || password === "") {
+  if (email === "" || password === "") {
     // Render 'missing credentials'
-    return res.render("login", { errorMsg: "Missing credentials."});
+    return res.render("login", { errorMsg: "Missing credentials." });
   }
 
   // use sample "user" (declared above)
-  if(email === user.email && password === user.password){
+  if (email === user.email && password === user.password) {
     // Add the user on the session and redirect them to the dashboard page.
     req.session.user = {
-      email: user.email
+      email: user.email,
     };
 
     res.redirect("/dashboard");
   } else {
     // render 'invalid username or password'
-    res.render("login", { errorMsg: "invalid email or password!"});
+    res.render("login", { errorMsg: "invalid email or password!" });
   }
 });
 
-
 // initialize ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 app.use(express.static("public"));
-
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
 
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
-};
+}
 
-app.use(function(req,res){
+app.use(function (req, res) {
   res.status(404).send("Page Not Found");
 });
-
